c37803ed0f4748093fdc8720e39422234172809e
zpskek <wjdghdwns0@gmail.com>
2021-02-14 19:06:29 +0900

    Networking with Axios Instance

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "Components/App";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
